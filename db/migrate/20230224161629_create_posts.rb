bec1763325e4e20d25fa9c02119ed76bcff473c3
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-24 20:01:10 +0100

    Add indexes to foreign key

--- db/migrate/20230224161629_create_posts.rb
+++ db/migrate/20230224161629_create_posts.rb
@@ -6,6 +6,12 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :text
       t.integer :commentscounter
       t.integer :likescounter
+      add_foreign_key :posts, :users, column: :authorid
+      add_foreign_key :posts, :comments, column: :commentscounter
+      add_foreign_key :posts, :likes, column: :likescounter
+      add_index :posts, :authorid
+      add_index :posts, :commentscounter
+      add_index :posts, :likescounter
 
       t.timestamps
     end
--- db/migrate/20230224162131_create_comments.rb
+++ db/migrate/20230224162131_create_comments.rb
@@ -4,6 +4,10 @@ class CreateComments < ActiveRecord::Migration[7.0]
       t.integer :authorid
       t.integer :postid
       t.text :text
+      add_foreign_key :comments, :users, column: :authorid
+      add_foreign_key :comments, :posts, column: :postid
+      add_index :comments, :authorid
+      add_index :comments, :postid
 
       t.timestamps
     end

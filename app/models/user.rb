6ad95183801225051fe0b132f1e74efb590ab0d1
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-25 14:19:03 +0100

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
+  validates :postscounter, presence: true
 end

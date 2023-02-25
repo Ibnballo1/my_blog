91e35f6a3e3a155648bfae53f8ab6aff93100c4d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-25 14:21:22 +0100

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
-class Post < ActiveRecord::Base
-  belongs_to :user
+class Post < ApplicationRecord
+  belongs_to :author, :class_name => 'User'
   has_many :likes
   has_many :comments
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :commentscounter, presence: true
+  validates :likescounter, presence: true
 end

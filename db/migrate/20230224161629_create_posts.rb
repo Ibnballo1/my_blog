18382bf3598c946a156ec1f0beab68540ce98e56
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-28 15:36:04 +0100

    Change authorid to author_id

--- db/migrate/20230224161629_create_posts.rb
+++ db/migrate/20230224161629_create_posts.rb
@@ -1,15 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :authorid
+      t.integer :author_id
       t.string :title
       t.text :text
       t.integer :commentscounter
       t.integer :likescounter
-      add_foreign_key :posts, :users, column: :authorid
+      add_foreign_key :posts, :users, column: :author_id
       add_foreign_key :posts, :comments, column: :commentscounter
       add_foreign_key :posts, :likes, column: :likescounter
-      add_index :posts, :authorid
+      add_index :posts, :author_id
       add_index :posts, :commentscounter
       add_index :posts, :likescounter
 

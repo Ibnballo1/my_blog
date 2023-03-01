c2ab15994b4d92a61a3e6250838a6a43f4e97b9c
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-01 14:38:38 +0100

    Add method to update post counter and method for most recent comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,12 @@ class Post < ApplicationRecord
   validates :text, presence: true
   validates :commentscounter, presence: true
   validates :likescounter, presence: true
+
+  def update_post_counter(user)
+    user.author.postscounter = user.author.posts.count
+  end
+
+  def five_most_recent_comment(post)
+    post.comments.order(created_at: :desc).limit(5)
+  end
 end

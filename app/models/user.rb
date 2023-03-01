1f6dfb1d3c80d38c67d9d1924a78e8919bf02e92
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-01 22:48:15 +0100

    Fix required changes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
-  has_many :likes
-  has_many :comments
+  has_many :likes, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
   validates :photo, presence: true
   validates :bio, presence: true
   validates :postscounter, presence: true
 
-  def self.three_most_recent_posts(author)
-    user = User.find_by(name: author)
-    user.posts.order(created_at: :desc).limit(3)
+  def three_most_recent_posts
+    posts.order('created_at desc').limit(3)
   end
 end

7baac75e7318f61aae390b46bdc21e8eb7ac8564
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-01 22:54:15 +0100

    Fix required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,14 +9,16 @@ class Post < ApplicationRecord
   validates :commentscounter, presence: true
   validates :likescounter, presence: true
 
-  def self.update_post_counter(author)
-    user = User.find_by(name: author)
-    user.postscounter = user.posts.count
-    user.save
-  end
+  after_save :update_post_counter
 
   def self.five_most_recent_comment(title)
     post = Post.find_by(title:)
     post.comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end

b9eadfcceacd3f63c2089712bd692dc56e025273
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-02 10:11:08 +0100

    Add validation test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  user {
+    User.new(name: 'Ibnballo', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher', postscounter: 0)
+  }
+
+  before { user.save }
+
+  it 'name should be present' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
 end

2d96cff264bdd4c6331b30e8cba877bd118f4320
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-02 15:17:19 +0100

    Fix linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   let(:user) { User.create(name: 'Tom') }
-  let(:post) { Post.new(author: user, text: 'This is my first post', likescounter: 3) }
+  let(:post) { Post.new(author: user, text: 'This is my first post', likescounter: 4) }
   let(:like) { Like.new(post:, author: user) }
 
   it 'increments the post comment counter' do
     post.increment!(:likescounter)
-    expect(post.likescounter).to eq(4)
+    expect(post.likescounter).to eq(5)
   end
 end

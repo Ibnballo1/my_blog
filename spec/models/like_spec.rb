1a9c135c21e22d610c03d04f864eba89bbe6cde5
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-02 14:47:34 +0100

    Add test to like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user) { User.create(name: 'Tom') }
+  let(:post) { Post.new(author: user, text: 'This is my first post', likescounter: 3) }
+  let(:like) { Like.new(post:, author: user) }
+
+  it 'increments the post comment counter' do
+    post.increment!(:likescounter)
+    expect(post.likescounter).to eq(4)
+  end
 end

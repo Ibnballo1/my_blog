1c72af35f33fdb399d078e02fd4d3fe7c404f7dc
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-02 04:09:17 +0100

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :photo, presence: true
-  validates :bio, presence: true
-  validates :postscounter, presence: true
+  validates :postscounter, comparison: { greater_than_or_equal_to: 0 }
 
   def three_most_recent_posts
     posts.order('created_at desc').limit(3)

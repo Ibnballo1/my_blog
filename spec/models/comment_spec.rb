977bb2274bd3972bc688686310b101b35fe558fc
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-02 14:42:51 +0100

    Add test to comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) { User.create(name: 'Tom') }
+  let(:post) { Post.new(author: user, text: 'This is my first post', commentscounter: 1) }
+  let(:comment) { Comment.new(post:, author: user, text: 'Hi Tom!') }
+
+  it 'increments the post comment counter' do
+    post.increment!(:commentscounter)
+    expect(post.commentscounter).to eq(2)
+  end
 end

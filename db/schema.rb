6797942f54f1e1d3d05b2765a2ca52cebaf76564
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-03 15:05:40 +0100

    Add new schema and tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_095840) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,8 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_095840) do
     t.integer "author_id"
     t.string "title"
     t.text "text"
-    t.integer "commentscounter"
-    t.integer "likescounter"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_095840) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "postscounter"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

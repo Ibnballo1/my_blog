dbc0a983d44ca65fad89a6aacb06cded961f81aa
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-03 20:59:44 +0100

    Update

--- db/migrate/20230303194137_add_foreign_key_and_index_to_comments.rb
+++ db/migrate/20230303194137_add_foreign_key_and_index_to_comments.rb
@@ -2,8 +2,9 @@ class AddForeignKeyAndIndexToComments < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :user, null: false, foreign_key: true
     add_reference :comments, :post, null: false, foreign_key: true
-    add_column :comments, :author_id, :integer
-    add_column :comments, :post_id, :integer
+
+    add_foreign_key :comments, :user, column: :author_id
+    add_foreign_key :comments, :posts, column: :post_id
     add_index :comments, :post_id
   end
 end

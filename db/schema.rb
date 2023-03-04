eb558110b16f7f27200173cd99cb2f8a237e54b5
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-04 06:07:21 +0100

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_195036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["comments_counter"], name: "index_posts_on_comments_counter"
+    t.index ["likes_counter"], name: "index_posts_on_likes_counter"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +52,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_063918) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "comments", column: "comments_counter"
+  add_foreign_key "posts", "likes", column: "likes_counter"
+  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "users", "posts", column: "posts_counter"
 end

2e195c65dd5f72a4782f51b202e23368f86b4573
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-04 07:07:39 +0100

    Edit migration files

--- db/migrate/20230303194832_add_foreign_key_and_index_to_posts.rb
+++ db/migrate/20230303194832_add_foreign_key_and_index_to_posts.rb
@@ -3,8 +3,5 @@ class AddForeignKeyAndIndexToPosts < ActiveRecord::Migration[7.0]
     add_foreign_key :posts, :users, column: :author_id
     add_foreign_key :posts, :comments, column: :comments_counter
     add_foreign_key :posts, :likes, column: :likes_counter
-
-    add_index :posts, :comments_counter
-    add_index :posts, :likes_counter
   end
 end

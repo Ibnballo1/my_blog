50de0405cce1c813452948dfeddafc300b01dd8d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-04 10:21:39 +0100

    Update increments for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,10 @@ class Comment < ApplicationRecord
 
   validates :text, presence: true
 
+  after_save :update_comments_counter
+
+  private
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end

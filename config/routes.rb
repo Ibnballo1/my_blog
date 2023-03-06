199817fbb80be64f8d048dc1cc17db189eb2b6cf
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-06 14:49:30 +0100

    Add routes to show post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'user/', to: 'user#index'
+  get 'user/:id', to: 'user#show'
+  get 'user/:id/posts', to: 'user#show'
+  get 'user/:id/posts/:id', to: 'user#show'
 
   # Defines the root path route ("/")
   # root "articles#index"

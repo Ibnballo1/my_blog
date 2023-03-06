7833128380e4a0f88d8d85218cad028532287a06
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-06 15:57:51 +0100

    Update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   # to get all users
   def index
-    @user = User.all
+    @users = User.all
   end
 
   # To get a specific user and
   # to get a post from a particular user
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
-    @post = @posts.id
+  end
+
+  def new
+    @user = User.new
+    @posts = Post.all
+  end
+
+  def show_post
+    @post = Post.find(params[:id])
   end
 end

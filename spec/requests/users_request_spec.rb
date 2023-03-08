02945657b6f62e50077fbc6f265af6df630acce1
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-08 15:01:12 +0100

    Update users controller test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Here is a list of all users')
     end
   end
+
+  describe 'users#show' do
+    # user = User.find(18)
+    before :each do
+      @user = User.create(
+        name: 'Keenan',
+        photo: 'photo',
+        bio: 'Best golfer ever',
+        posts_counter: 1
+      )
+      get "/users/#{@user.id}"
+    end
+  
+    it 'is correct' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'shows #show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'shows correct body placeholder' do
+      expect(response.body).to include('Here is a user with this id')
+    end
+  end
 end

102b1e54e746a9832ad3ba731e5af8f6a487f5c3
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-08 18:18:44 +0100

    Add tests to post controller

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'posts#index' do
+    # before(:example) { get posts_path }
+    before :each do
+      @user = User.create(
+        name: 'Keenan',
+        photo: 'photo',
+        bio: 'Best golfer ever',
+        posts_counter: 1
+      )
+      get "/users/#{@user.id}/posts"
+    end
+    it 'is correct' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'shows #index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'shows correct body placeholder' do
+      expect(response.body).to include('Here is a list of all posts of a certain user')
+    end
+  end
+
+  describe 'posts#show' do
+    before :each do
+      @user = User.create(
+        name: 'Keenan',
+        photo: 'photo',
+        bio: 'Best golfer ever',
+        posts_counter: 1
+      )
+      @post = Post.create(
+        author: @user,
+        author_id: @user.id,
+        title: 'Hello',
+        text: 'This is my first post',
+        comments_counter: 0,
+        likes_counter: 0,
+        user_id: 0
+      )
+      get "/users/author_id/posts/#{@post.id}"
+    end
+
+    it 'is correct' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'shows #index template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'shows correct body placeholder' do
+      expect(response.body).to include("<p>Here is a user's post with this id</p>")
+    end
+  end
+end

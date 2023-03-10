15cffd0bfb3b31e1fabff4c36ddf988764306d26
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-10 17:20:28 +0100

    Recreate database

--- db/migrate/20230308150659_add_user_id_to_posts.rb
+++ db/migrate/20230308150659_add_user_id_to_posts.rb
@@ -1,6 +1,8 @@
 class AddUserIdToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :user_id, :integer
-    change_column_null :posts, :user_id, true
+    unless column_exists?(:posts, :user_id)
+      add_column :posts, :user_id, :integer
+      change_column_null :posts, :user_id, true
+    end
   end
 end

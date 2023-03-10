baeea1274e6d05733d6cfe442088bc71eab40d79
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-10 08:00:42 +0100

    Add view for the posts of a specific user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.all
+    @users = User.find(params[:user_id])
+    @posts = @users.posts
   end
 
   def show
     @post = Post.find(params[:id])
+    # @comments = @post.comments.includes(:user)
+    # @users = @comments.map(&:user).uniq
   end
 end

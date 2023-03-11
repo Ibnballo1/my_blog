36479b21792cd86a6a80bfeb33101b1f5f1bdddc
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-11 10:37:19 +0100

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,25 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    # @comments = @post.comments.includes(:user)
-    # @users = @comments.map(&:user).uniq
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end

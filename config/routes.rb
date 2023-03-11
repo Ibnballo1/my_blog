ac71db687ee70121e81a97835cfe1c6d8be822c7
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-11 11:18:35 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show]
+    resources :comments, only: %i[new create]
+    resources :likes, only: %i[create]
   end
 end

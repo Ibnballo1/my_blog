1a1630a0dae391b39f25de9aeef7e319f71b11af
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-15 08:45:19 +0100

    Add test for index

---
+++ spec/integration/user_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'User Index', type: :system do
+  describe 'index page' do
+    before(:each) do
+      @user = User.create(name: 'Ibnballo', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher', posts_counter: 0)
+    end
+
+    it 'should show I can see the username of all other users' do
+      visit users_path
+      expect(page).to have_content('List of all users')
+      expect(page).to have_selector('img')
+      expect(page).to have_content('Number of posts:')
+    end
+  end
+end

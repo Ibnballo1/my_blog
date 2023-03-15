6723135577258a2bd2daf07872a7de08e94b42d3
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-15 10:29:19 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_150659) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_092042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150659) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150659) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150659) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "users"
 end

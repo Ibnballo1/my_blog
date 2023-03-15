b21623ce130e66533d09ae96fe5ee738a612a21f
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-15 17:37:43 +0100

    Fix errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_150659) do
+ActiveRecord::Schema[7.0].define(version: 20_230_308_150_659) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'comments', force: :cascade do |t|
+    t.integer 'author_id'
+    t.integer 'post_id'
+    t.text 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'likes', force: :cascade do |t|
+    t.integer 'author_id'
+    t.integer 'post_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
-    t.string "title"
-    t.text "text"
-    t.integer "comments_counter", default: 0
-    t.integer "likes_counter", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'posts', force: :cascade do |t|
+    t.integer 'author_id'
+    t.string 'title'
+    t.text 'text'
+    t.integer 'comments_counter', default: 0
+    t.integer 'likes_counter', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'photo'
+    t.text 'bio'
+    t.integer 'posts_counter', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end

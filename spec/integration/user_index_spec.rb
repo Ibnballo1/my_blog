c75d10635cce5c8ad21f12f3740254772294a7dd
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-15 21:19:51 +0100

    Update user index test

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -6,11 +6,23 @@ RSpec.describe 'User Index', type: :system do
       @user = User.create(name: 'Ibnballo', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher', posts_counter: 0)
     end
 
-    it 'should show I can see the username of all other users' do
+    it 'should show the username of all users' do
       visit users_path
       expect(page).to have_content('List of all users')
+    end
+
+    it 'should show profile image' do
+      visit users_path
       expect(page).to have_selector('img')
+    end
+
+    it 'should show number of posts' do
+      visit users_path
       expect(page).to have_content('Number of posts:')
+    end
+
+    it 'should ridirect after clicking on a link' do
+      visit users_path
       click_on('Ibnballo')
       expect(page).to have_content('Teacher')
     end

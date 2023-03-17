b840ff87675a04a80d6f2ca9e17b5bfffe925a60
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-17 18:06:44 +0100

    Fix errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,12 +65,12 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'ibnballo@gmail.com',
-    password:             '@Ajibaye1990',
-    authentication:       'plain',
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'ibnballo@gmail.com',
+    password: '@Ajibaye1990',
+    authentication: 'plain',
     enable_starttls_auto: true
   }
 

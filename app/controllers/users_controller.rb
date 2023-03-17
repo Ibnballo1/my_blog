3df34616ceeccb1f2f478b5c25701cfdb816b678
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-17 11:31:37 +0100

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  # to get all users
   def index
     @users = User.all
   end
 
-  # To get a specific user and
-  # to get a post from a particular user
   def show
     @user = User.find(params[:id])
-    @posts = Post.all
   end
 end
